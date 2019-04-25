82e355e51f94b83c5b26965b17a4d792c29b85d2
Calum Webb <calum@paceup.com>
2019-04-25 21:11:16 +0100

    started client

---
+++ client/settings.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+load_dotenv()
+
+load_dotenv(verbose=True)
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+class Configuration(object):
+    BASE_DIR = os.path.abspath(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
+    )
+
---
+++ client/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+import settings
+
+setup(
+    name="pychat-client",
+    author="Calum Webb",
+    packages=find_packages(exclude=["tests", "alembic", "build", "dist"]),
+)
